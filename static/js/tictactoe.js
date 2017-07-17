aa6acdb28ea42609bb3d97d0a3b7a865b81c7591
Mark Sia Hansheng <mark@tigerlab.com>
2017-07-17 18:12:34 +0800

    Add class checked to td and remove class in gameReset

--- static/js/tictactoe.js
+++ static/js/tictactoe.js
@@ -1,237 +1,243 @@
-	// Board
-	// 0 1 2
-	// 3 4 5
-	// 6 7 8
+  // Board
+  // 0 1 2
+  // 3 4 5
+  // 6 7 8
 
-	$(document).ready(function() {
-		var clickDisabled = false;
-		var cellClickDisabled = true;
-		$(".selectX").click(function() {
-			if (!clickDisabled) {
-				$(".selectX h2").css("text-decoration", "underline");
-				humanToken = "X";
-				aiToken = "O";
-				console.log("aiToken" + " " + aiToken)
-				clickDisabled = true;
-				cellClickDisabled = false;
-			}
-		});
-		$(".selectO").click(function() {
-			if (!clickDisabled) {
-				$(".selectO h2").css("text-decoration", "underline");
-				humanToken = "O";
-				aiToken = "X";
-				console.log("aiToken" + " " + aiToken)
-				clickDisabled = true;
-				cellClickDisabled = false;
-			}
-		});
+  $(document).ready(function() {
+    var clickDisabled = false;
+    var cellClickDisabled = true;
+    $(".selectX").click(function() {
+      if (!clickDisabled) {
+        $(".selectX h2").css("text-decoration", "underline");
+        humanToken = "X";
+        aiToken = "O";
+        console.log("aiToken" + " " + aiToken)
+        clickDisabled = true;
+        cellClickDisabled = false;
+      }
+    });
+    $(".selectO").click(function() {
+      if (!clickDisabled) {
+        $(".selectO h2").css("text-decoration", "underline");
+        humanToken = "O";
+        aiToken = "X";
+        console.log("aiToken" + " " + aiToken)
+        clickDisabled = true;
+        cellClickDisabled = false;
+      }
+    });
 
-		$("td").click(function() {
-			if (!cellClickDisabled) {
-				turn = humanToken
-				handleClick(this);
-			}
-		});
-	});
 
-	var board = [
-	"_", "_", "_",
-	"_", "_", "_",
-	"_", "_", "_"
-	]
-	var winningConditions = [
-	[0, 1, 2 ], [3, 4 ,5], [6, 7 ,8],
-	[0, 3, 6], [1, 4, 7], [2, 5 ,8],
-	[0, 4, 8], [2, 4, 6],
-	]
+    $("td").each(function(){
+      $(this).click(function(){
+        if(!$(this).hasClass("checked") && !cellClickDisabled){
+          turn = humanToken
+          handleClick(this);
+        }
+      });
+    });
+  });
 
-	var turn;
+  var board = [
+  "_", "_", "_",
+  "_", "_", "_",
+  "_", "_", "_"
+  ]
+  var winningConditions = [
+  [0, 1, 2 ], [3, 4 ,5], [6, 7 ,8],
+  [0, 3, 6], [1, 4, 7], [2, 5 ,8],
+  [0, 4, 8], [2, 4, 6],
+  ]
 
-	var round = 0;
-	var humanToken;
-	var aiToken;
-	var iter = 0;
+  var turn;
 
-	// token1 = "X"
-	// token2 = "O"
+  var round = 0;
+  var humanToken;
+  var aiToken;
+  var iter = 0;
 
-	// Check if current board matches with winning condition
-	function checkForWin(board) {
-		for (var i = 0; i < winningConditions.length; i++) {
+  // token1 = "X"
+  // token2 = "O"
 
-			var token1 = board[winningConditions[i][0]]
-			var token2 = board[winningConditions[i][1]]
-			var token3 = board[winningConditions[i][2]]
+  // Check if current board matches with winning condition
+  function checkForWin(board) {
+    for (var i = 0; i < winningConditions.length; i++) {
 
-	    // win condition met
-	    if (token1 === token2 && token1 === token3) {
-	    	if (token1 == aiToken) {
-	    		return "AI_WINS"
-	    	} else if (token1 == humanToken) {
-	    		return "HUMAN_WINS"
-	    	}
+      var token1 = board[winningConditions[i][0]]
+      var token2 = board[winningConditions[i][1]]
+      var token3 = board[winningConditions[i][2]]
 
-	    }
+      // win condition met
+      if (token1 === token2 && token1 === token3) {
+        if (token1 == aiToken) {
+          return "AI_WINS"
+        } else if (token1 == humanToken) {
+          return "HUMAN_WINS"
+        }
 
-	}
+      }
 
-	  // If no win, and no draw.
-	  return false;
-	}
+  }
 
-	function resetGame() {
-		round = 0;
-		board = [
-		"_", "_", "_",
-		"_", "_", "_",
-		"_", "_", "_"
-		]
-		$("td").html("");
-		// $(".selectX h2").css("text-decoration", "none");
-		// $(".selectO h2").css("text-decoration", "none");
-	}
+    // If no win, and no draw.
+    return false;
+  }
 
-	// Obtain remaining available spots in an array
-	function getAvailableSpots(board) {
-		var availableSpotsArray = [];
+  function resetGame() {
+    round = 0;
+    board = [
+    "_", "_", "_",
+    "_", "_", "_",
+    "_", "_", "_"
+    ]
+    $("td").html("");
+    $("td").removeClass("checked");
+    // $(".selectX h2").css("text-decoration", "none");
+    // $(".selectO h2").css("text-decoration", "none");
+  }
 
-		for (var i = 0; i < board.length; i++) {
-			if (board[i] == "_") {
-				availableSpotsArray.push(i);
-			}
-		}
-		return availableSpotsArray;
-	}
+  // Obtain remaining available spots in an array
+  function getAvailableSpots(board) {
+    var availableSpotsArray = [];
 
-	function handleClick(cell) {
+    for (var i = 0; i < board.length; i++) {
+      if (board[i] == "_") {
+        availableSpotsArray.push(i);
+      }
+    }
+    return availableSpotsArray;
+  }
 
-		if (board[cell.id] == "_") {
-			$(cell).html(humanToken)
-			board[cell.id] = humanToken
-		}
+  function handleClick(cell) {
 
-	  // Check if win condition met
-	  gameEndCheck(board);
+    if (board[cell.id] == "_") {
+      $(cell).addClass('checked')
+      $(cell).html(humanToken)
+      board[cell.id] = humanToken
+    }
 
-  	var index = getBestMove(board)
-  	var selector = "#" + index;
-  	$(selector).html(aiToken);
-  	board[index] = aiToken;
+    // Check if win condition met
+    gameEndCheck(board);
+
+    var index = getBestMove(board)
+    var selector = "#" + index;
+    $(selector).html(aiToken);
+    $(selector).addClass('checked');
+    board[index] = aiToken;
 
     gameEndCheck(board);
 
 
-	}
+  }
 
-	function gameEndCheck(board) {
-		if (checkForWin(board) == "HUMAN_WINS") {
-	  	setTimeout(function () {
-	  		alert ("You won the game against the AI!");
-	  		resetGame();
-	  	}, 400)
+  function gameEndCheck(board) {
+    if (checkForWin(board) == "HUMAN_WINS") {
+      setTimeout(function () {
+        alert ("You won the game against the AI!");
+        resetGame();
+      }, 400)
 
-	  } else if (checkForWin(board) == "AI_WINS") {
-	  	setTimeout(function () {
-	  		alert ("You lost, please try again...");
-	  		resetGame();
-	  	}, 400)
-	  } else if (checkMovesLeft(board) == false) {
-	  	setTimeout(function () {
-	  		alert ("You drew against the AI.");
-	  		resetGame();
-	  	}, 400)
-	  }
-	}
+    } else if (checkForWin(board) == "AI_WINS") {
+      setTimeout(function () {
+        alert ("You lost, please try again...");
+        resetGame();
+      }, 400)
+    } else if (checkMovesLeft(board) == false) {
+      setTimeout(function () {
+        alert ("You drew against the AI.");
+        resetGame();
+      }, 400)
+    }
+  }
 
-	function checkMovesLeft(board){
-	    for (var i = 0; i<board.length; i++){
-	        if (board[i] == '_'){
-	            return true;
-	        }
-	    }
-	return false;
-	}  
+  function checkMovesLeft(board){
+      for (var i = 0; i<board.length; i++){
+          if (board[i] == '_'){
+              return true;
+          }
+      }
+  return false;
+  }  
 
-	function minimax(board, depth, isMaximizer) {
-		var availableSpotsArray = getAvailableSpots(board)
+  function minimax(board, depth, isMaximizer) {
+    var availableSpotsArray = getAvailableSpots(board)
 
-		if (checkForWin(board) == "AI_WINS") {
-			return 10 - depth;
-		} else if (checkForWin(board) == "HUMAN_WINS"){
-			return depth -10;
-		} else if (checkMovesLeft(board) == false) {
-		    // Game draw, return 0
-		    return 0;
-		}
+    if (checkForWin(board) == "AI_WINS") {
+      return 10 - depth;
+    } else if (checkForWin(board) == "HUMAN_WINS"){
+      return depth -10;
+    } else if (checkMovesLeft(board) == false) {
+        // Game draw, return 0
+        return 0;
+    }
 
-		if (isMaximizer) {
-			var bestScore = -10000;
+    if (isMaximizer) {
+      var bestScore = -10000;
 
-		    // Go through all empty cells
-		    for (var i=0; i < board.length; i++) {
-		    	var boardCopy = board.slice()
-		    	if (boardCopy[i] == "_") {
+        // Go through all empty cells
+        for (var i=0; i < board.length; i++) {
+          var boardCopy = board.slice()
+          if (boardCopy[i] == "_") {
 
-		    		boardCopy[i] = aiToken;
+            boardCopy[i] = aiToken;
 
-		    		bestScore = Math.max(bestScore, minimax(boardCopy, depth+1, false))
+            bestScore = Math.max(bestScore, minimax(boardCopy, depth+1, false))
 
-		    		boardCopy[i] = "_"
+            boardCopy[i] = "_"
 
-		    	}
-		    }
+          }
+        }
 
-		    return bestScore;
+        return bestScore;
 
-		} else {
+    } else {
 
-			var bestScore = 10000;
+      var bestScore = 10000;
 
-		    // Go through all empty cells
-		    for (var i=0; i < board.length; i++) {
-		    	var boardCopy = board.slice()
-		    	if (boardCopy[i] == "_") {
+        // Go through all empty cells
+        for (var i=0; i < board.length; i++) {
+          var boardCopy = board.slice()
+          if (boardCopy[i] == "_") {
 
-		    		boardCopy[i] = humanToken;
+            boardCopy[i] = humanToken;
 
-		    		bestScore = Math.min(bestScore, minimax(boardCopy, depth+1, true))
+            bestScore = Math.min(bestScore, minimax(boardCopy, depth+1, true))
 
-		    		boardCopy[i] = "_"
-		    	}
-		    }
+            boardCopy[i] = "_"
+          }
+        }
 
-		    return bestScore;
+        return bestScore;
 
-		}
+    }
 
-	}
+  }
 
-	function getBestMove(board) {
+  function getBestMove(board) {
 
-		var highestValue = -1000
-		var bestMove = null;
+    var highestValue = -1000
+    var bestMove = null;
 
-		for (var i=0; i < board.length; i++) {
-			var boardCopy = board.slice()
-			if (boardCopy[i] == "_") {
+    for (var i=0; i < board.length; i++) {
+      var boardCopy = board.slice()
+      if (boardCopy[i] == "_") {
 
-				boardCopy[i] = aiToken;
+        boardCopy[i] = aiToken;
 
-				moveValue = minimax(boardCopy, 0, false)
+        moveValue = minimax(boardCopy, 0, false)
 
-				boardCopy[i] = "_"
+        boardCopy[i] = "_"
 
-				if (moveValue > highestValue) {
-					highestValue = moveValue
-					bestMove = i;
-				}
-			}
-		}
+        if (moveValue > highestValue) {
+          highestValue = moveValue
+          bestMove = i;
+        }
+      }
+    }
 
-		console.log("Best move", bestMove)
-		console.log("highestvalue", highestValue)
+    console.log("Best move", bestMove)
+    console.log("highestvalue", highestValue)
 
-		return bestMove
+    return bestMove
 
-	}
+  }
